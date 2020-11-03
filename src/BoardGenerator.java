9de43d1575e89f6927c37cdbe2271a37564f4aad
Abdulcube <abdulrahman.shaaeldin@gmail.com>
2020-11-02 20:50:25 -0500

    Traverse Method updated

--- src/BoardGenerator.java
+++ src/BoardGenerator.java
@@ -1,9 +1,9 @@
 
 public class BoardGenerator {
-	
+
 	Node[][] Board;
 	int dimension; //(3x3) (6x6) (9x9)
-	
+
 	//Default Constructor
 	public BoardGenerator() {
 		Board = new Node[3][3];
@@ -13,7 +13,7 @@ public class BoardGenerator {
 		genPits();
 		genPieces();
 	}
-	
+
 	//Constructor for dimensions
 	public BoardGenerator(int d) {
 		if( d == 3 || d == 6 || d == 9 ) {
@@ -22,7 +22,7 @@ public class BoardGenerator {
 			System.out.println("ERROR/ cannot continue");
 			System.exit(0);
 		}
-		
+
 		Board = new Node[d][d];
 		//...
 		init();
@@ -49,7 +49,16 @@ public class BoardGenerator {
 		}
 		return null;
 	}
-	
+
+	public void traverse() {
+		for(int i = 0; i < dimension; i ++) {
+			for( int k = 0; k < dimension; k ++) {
+				System.out.println("");
+			}
+		}
+		return;
+	}
+
 	//Initializer for non-nulling Nodes in the list
 	public void init() {
 		for(int i = 0; i < dimension; i ++) {
@@ -59,147 +68,147 @@ public class BoardGenerator {
 			}
 		}
 	}
-	
+
 	//generate the Pits for the given board
 	public void genPits() {
-		 
+
 		for(int i = 1; i < dimension-1; i ++) {
-			
+
 			int u = dimension;
 			int l = 1;
 			int r = (int) (Math.random() * (u - l)) + l;
-			
-			
+
+
 			this.Board[r][i].isPit = true;
-			
+
 		}
 	}
-	
-	//Possibly make it more random 
+
+	//Possibly make it more random
 	public void genPieces() {
 		if        (dimension == 3) {
 			//Away Peices on board
 			Board[0][0].type = 'w';
 			Board[0][0].side = 1;
-			
+
 			Board[1][0].type = 'h';
 			Board[1][0].side = 1;
-			
+
 			Board[2][0].type = 'm';
 			Board[2][0].side = 1;
-			
-			
+
+
 			//Home Peices on board
 			Board[0][2].type = 'w';
 			Board[0][2].side = 0;
-			
+
 			Board[1][2].type = 'h';
 			Board[1][2].side = 0;
-			
+
 			Board[2][2].type = 'm';
 			Board[2][2].side = 0;
-			
+
 		} else if (dimension == 6) {
 			//Away Peices on board
 			Board[0][0].type = 'w';
 			Board[0][0].side = 1;
-			
+
 			Board[1][0].type = 'h';
 			Board[1][0].side = 1;
-			
+
 			Board[2][0].type = 'm';
 			Board[2][0].side = 1;
-			
+
 			Board[3][0].type = 'w';
 			Board[3][0].side = 1;
-			
+
 			Board[4][0].type = 'h';
 			Board[4][0].side = 1;
-			
+
 			Board[5][0].type = 'm';
 			Board[5][0].side = 1;
-			
-			
+
+
 			//Home Peices on board
 			Board[0][5].type = 'w';
 			Board[0][5].side = 0;
-			
+
 			Board[1][5].type = 'h';
 			Board[1][5].side = 0;
-			
+
 			Board[2][5].type = 'm';
 			Board[2][5].side = 0;
-			
+
 			Board[3][5].type = 'w';
 			Board[3][5].side = 0;
-			
+
 			Board[4][5].type = 'h';
 			Board[4][5].side = 0;
-			
+
 			Board[5][5].type = 'm';
 			Board[5][5].side = 0;
-			
-			
+
+
 		} else if (dimension == 9) {
-			
+
 			//Away Peices on board
 			Board[0][0].type = 'w';
 			Board[0][0].side = 1;
-			
+
 			Board[1][0].type = 'h';
 			Board[1][0].side = 1;
-			
+
 			Board[2][0].type = 'm';
 			Board[2][0].side = 1;
-			
+
 			Board[3][0].type = 'w';
 			Board[3][0].side = 1;
-			
+
 			Board[4][0].type = 'h';
 			Board[4][0].side = 1;
-			
+
 			Board[5][0].type = 'm';
 			Board[5][0].side = 1;
-			
+
 			Board[6][0].type = 'w';
 			Board[6][0].side = 1;
-			
+
 			Board[7][0].type = 'h';
 			Board[7][0].side = 1;
-			
+
 			Board[8][0].type = 'm';
 			Board[8][0].side = 1;
-			
-			
+
+
 			//Home Peices on board
 			Board[0][8].type = 'w';
 			Board[0][8].side = 0;
-			
+
 			Board[1][8].type = 'h';
 			Board[1][8].side = 0;
-			
+
 			Board[2][8].type = 'm';
 			Board[2][8].side = 0;
-			
+
 			Board[3][8].type = 'w';
 			Board[3][8].side = 0;
-			
+
 			Board[4][8].type = 'h';
 			Board[4][8].side = 0;
-			
+
 			Board[5][8].type = 'm';
 			Board[5][8].side = 0;
-			
+
 			Board[6][8].type = 'w';
 			Board[6][8].side = 0;
-			
+
 			Board[7][8].type = 'h';
 			Board[7][8].side = 0;
-			
+
 			Board[8][8].type = 'm';
 			Board[8][8].side = 0;
-			
+
 		}
-		
+
 	}
 }
