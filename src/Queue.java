08dfa7a9d07eb32c2635ec4922c2abb78d6b62de
BMoran32 <58581849+BMoran32@users.noreply.github.com>
2020-11-03 22:27:51 -0500

    Update Queue.java

--- src/Queue.java
+++ src/Queue.java
@@ -1,47 +1,70 @@
 import java.io.*;
 import java.util.*;
+
 public class Queue {
-  List<Tree> head;
-  int size;
-  public Queue (){
-    head = null;
-  }
+	List<State> head;
+	int size;
 
-  public void add(Tree t){
-    if(head == null){
-      head = new ArrayList<Tree>();
-      head.add(t);
-      size = 1;
-      return;
-    }
-    for(int counter = 0; counter < head.size(); counter++){
-      if(head.get(counter).h <=t.h){
-        head.add(counter,t);
-        size = head.size();
-        return;
-      }
-    }
-    head.add(t);
+	public Queue() {
+		head = null;
+		size = 0;
+	}
 
-  }
+	public void add(State t) {
+		if (head == null) {
+			head = new ArrayList<State>();
+			head.add(t);
+			size = 1;
+			return;
+		}
+		for (int counter = 0; counter < head.size(); counter++) {
+			if (head.get(counter).h <= t.h) {
+				head.add(counter, t);
+				size = head.size();
+				return;
+			}
+		}
+		head.add(t);
 
-  public void traverse(){
-    for(int counter = 0; counter < head.size(); counter++){
-      if(head.get(counter) != null){
-        System.out.print("" + head.get(counter).h+ "");
-      }
-    }
-  }
-  public Tree pop(){
-    if(head == null){
-      return null;
-    }
-    Tree res = head.get(0);
-    head.remove(0);
-    size--;
-    if(size == 0){
-      head = null;
-    }
-    return res;
-  }
+	}
+
+	public void traverse() {
+		for (int counter = 0; counter < size; counter++) {
+			if (head.get(counter) != null) {
+				System.out.print("" + head.get(counter).h + "");
+			}
+		}
+	}
+
+	public State pop() {
+		if (head == null) {
+			return null;
+		}
+		State res = head.get(0);
+		head.remove(0);
+		size--;
+		if (size == 0) {
+			head = null;
+		}
+		return res;
+	}
+	
+	public State popsB() {
+		if (head == null) {
+			return null;
+		}
+		State res = head.get(size-1);
+		head.remove(size -1);
+		size--;
+		if (size == 0) {
+			head = null;
+		}
+		return res;
+	}
+	public boolean isEmpty() {
+		if( size == 0) {
+			return true;
+		}
+		return false;
+	}
 }
